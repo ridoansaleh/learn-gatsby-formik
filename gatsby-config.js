ce41b95728def895b186d7baaef93013088ccb3c
ridoansaleh <ridoansalehnasution98@gmail.com>
2019-10-24 23:19:25 +0700

    adjust deployment config

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
+  pathPrefix: "/learn-gatsby-formik",
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
@@ -31,5 +32,4 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
-  pathPrefix: "/learn-gatsby-formik",
 }
